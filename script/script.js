35c18f93e9a1253fcb7922467511d3ea913f0f40
diate.com <diate.rpps@gmail.com>
2026-03-31 21:14:58 -0300

    Ajustando logica do projeto

--- script/script.js
+++ script/script.js
@@ -7,6 +7,8 @@ const form = document.querySelector("form")
 const exchangeRate = document.querySelector("exchange-rate")
 const convertedValue = document.querySelector("converted-value")
 
+
+
 //Variáveis de câmbio
 let dollar = 5.26 
 let euro = 6.04
@@ -36,5 +38,34 @@ currencySelect.addEventListener("change", () => {
 //
 form.addEventListener("submit", (event) =>{
     event.preventDefault()
+
+    let symbol = ""
+    let rate = 0
+
+    //Teste - Verificar o valor selecionado e a taxa de câmbio correspondente
+    console.log(`currencySelect.value: ${currencySelect.value}`)
+    console.log(`rate: ${rate}`)
+
+    switch (currencySelect.value) {
+        case "USD":
+            symbol = "US$"
+            rate = dollar
+            break
+        case "EUR":
+            symbol = "€"
+            rate = euro
+            break
+        case "GBP":
+            symbol = "£"
+            rate = pound
+            break
+    }
+
+    //Teste - Verificar o símbolo e a taxa de câmbio atribuídos
+    console.log(`${symbol} 1 = RS ${rate}`)
+    console.log(`R$ ${inputValue.value * rate} Reais`)
+
+    exchangeRate.textContent = `${symbol} 1 = R$ ${rate}`
+    convertedValue.textContent = `R$ ${inputValue.value * rate} Reais`
 
 })
